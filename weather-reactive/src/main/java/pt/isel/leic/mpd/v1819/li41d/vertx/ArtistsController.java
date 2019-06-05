a37cbfb06206485c00354914a361fd66d1270f8a
Luís Falcão <lfalcao@cc.isel.pt>
2019-06-05 14:47:51 +0100

    Lesson 27 - Vertx sending response in chunks.

--- weather-reactive/src/main/java/pt/isel/leic/mpd/v1819/li41d/vertx/ArtistsController.java
+++ weather-reactive/src/main/java/pt/isel/leic/mpd/v1819/li41d/vertx/ArtistsController.java
@@ -1,18 +1,44 @@
 package pt.isel.leic.mpd.v1819.li41d.vertx;
 
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.RoutingContext;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class ArtistsController {
+    private Vertx worker = Vertx.vertx(new VertxOptions().setWorkerPoolSize(1));
+
     public void searchArtists(RoutingContext routingContext) {
         HttpServerResponse response = routingContext.response();
         response.putHeader("content-type", "text/plain");
         response.setChunked(true);
 
-        response.write("Artist1\n");
+        worker.executeBlocking((f) -> {
+            for (int i = 0; i < 128; i++) {
+                response.write("Artist1");
+                sleep(100);
+                response.write("Artist2");
+            }
 
-        routingContext.vertx().setTimer(5000, tid -> response.write("Artist2\n"));
-        routingContext.vertx().setTimer(10000, tid -> response.end("Artist3\n"));
+            response.end("Artist3\n");
+            f.complete(); },
+                ar -> { });
 
+
+
+//        worker.setTimer(5000, tid -> response.write("Artist2\n"));
+//        worker.setTimer(10000, tid -> response.end("Artist3\n"));
+
+    }
+
+    private void sleep(int tout) {
+        try {
+            Thread.sleep(tout);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
